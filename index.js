62909105fe3267f111bd410fd04b16443f9adea7
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-04-29 19:03:37 +0100

    increment button added with increment functionality

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.getElement
+let countEl = document.getElementById("count-el");
+
+let count = 0; 
+
+const increment = () => {
+    count += 1;
+    countEl.innerText = count;
+    console.log(count)
+}
